ca6b9656849ef6d6d11dc9ddd982ae046437c847
Krystian Ochmański <krysochmanski1337@gmail.com>
2025-03-11 16:48:23 +0100

    add - data annotations

--- ZTP_Projekt_1.Web/DTOs/ProductDTOs/CreateProductDTO.cs
+++ ZTP_Projekt_1.Web/DTOs/ProductDTOs/CreateProductDTO.cs
@@ -1,17 +1,28 @@
-﻿namespace ZTP_Projekt_1.Web.DTOs.ProductDTOs
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZTP_Projekt_1.Web.DTOs.ProductDTOs
 {
     public class CreateProductDTO
     {
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Product name must have 3-20 characters.")]
+        [RegularExpression(@"^[A-Za-z0-9]+$", ErrorMessage = "Product name can only contain letters and numbers.")]
         public string Name { get; set; } = null!;
 
         public string? Description { get; set; }
 
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity can not be nagative.")]
         public int StockQuantity { get; set; }
 
         public bool IsAvailable { get; set; } = true;
 
+        [Required]
         public int CategoryId { get; set; }
     }
 }
--- ZTP_Projekt_1.Web/DTOs/ProductDTOs/EditProductDTO.cs
+++ ZTP_Projekt_1.Web/DTOs/ProductDTOs/EditProductDTO.cs
@@ -1,17 +1,31 @@
-﻿namespace ZTP_Projekt_1.Web.DTOs.ProductDTOs
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZTP_Projekt_1.Web.DTOs.ProductDTOs
 {
 	public class EditProductDTO
 	{
+        [Required]
 		public int Id { get; set; }
 
-		public string Name { get; set; } = null!;
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Product name must have 3-20 characters.")]
+        [RegularExpression(@"^[A-Za-z0-9]+$", ErrorMessage = "Product name can only contain letters and numbers.")]
+        public string Name { get; set; } = null!;
 
-		public string? Description { get; set; }
-	
-		public decimal Price { get; set; }
+        public string? Description { get; set; }
 
-		public int StockQuantity { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
 
-		public bool IsAvailable { get; set; } = true;
-	}
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity can not be nagative.")]
+        public int StockQuantity { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
 }
